db89c854965bd9895aada6bb55287bdc4755b80c
Peter Zvirinsky <peter.zvirinsky@gmail.com>
2016-12-04 20:35:20 +0200

    class 10

---
+++ src/class10/solution.pas
@@ -0,0 +1,33 @@
+program stack;
+
+const MAX = 100;
+
+type TData = char;
+
+type TStack = record
+  stack: array[1..MAX] of TData;
+  size: 0..MAX;
+end;
+
+function isEmpty(var stack: TStack): boolean;
+begin
+
+end;
+
+procedure init(var stack: TStack);
+begin
+end;
+
+procedure push(var stack: TStack; item: TData);
+begin
+
+end;
+
+function pop(var stack: TStack): TData;
+begin
+
+end;
+
+begin
+
+end.
